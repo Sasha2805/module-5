dfe513c34c686659c1506faa76730208fbdc9baa
Sasha Kaminska <sasha249133@ukr.net>
2017-10-01 23:37:18 +0300

    Created class CarWheel

--- src/Car/CarWheel.java
+++ src/Car/CarWheel.java
@@ -1,4 +1,38 @@
 package Car;
 
 public class CarWheel {
+    private double conditionWheel;
+
+    public CarWheel(){
+        conditionWheel = 1;
+    }
+
+    public CarWheel(double conditionWheel){
+        this.conditionWheel = conditionWheel;
+    }
+
+    public void changeWheel(){
+        conditionWheel = 1;
+    }
+
+    public void eraseWheel(double x){
+        conditionWheel %= x / 100;
+    }
+
+    public double condition(){
+        return conditionWheel;
+    }
+
+    public void show() {
+        System.out.println("Состояние целосности шины: " + conditionWheel);
+    }
+
+    public void setWheelCondition(double conditionWheel) {
+        this.conditionWheel = conditionWheel;
+    }
+
+    public double getWheelCondition() {
+        return conditionWheel;
+    }
+
 }
